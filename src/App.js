2293c6ed920a8bc421ab73c00d06783538a4a7a5
cheongu <kangsun0528@gmail.com>
2023-01-25 22:52:52 +0900

    Router 변경, 글로벌스타일 적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import Button from "./components/button/Button";
-
+import Router from "./shared/Router";
 import React from "react";
 
 const App = () => {
--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,20 +1,20 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Layout from "./../components/Layout";
 import Chat from "./../pages/Chat";
 import ChatList from "./../pages/ChatList";
 import FriendsList from "./../pages/FriendsList";
 import SignIn from "./../pages/SignIn";
 import SignUp from "./../pages/SignUp";
+import GlobalStyle from "./GlocalStyle";
 const Router = () => {
   return (
     <BrowserRouter>
+      <GlobalStyle />
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="/" element={<SignIn />}>
           <Route path="/friendsList" element={<FriendsList />} />
           <Route path="/chatList" element={<ChatList />} />
           <Route path="/chat" element={<Chat />} />
-          <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
         </Route>
       </Routes>

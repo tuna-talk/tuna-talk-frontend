8a9fff597f71f60c7c9039b1e0d4217fd59d302c
cheongu <kangsun0528@gmail.com>
2023-01-25 23:15:46 +0900

    router 변경

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Router from "./shared/Router";
 import React from "react";
 
 const App = () => {
-  return <Router></Router>;
+  return <Router />;
 };
 
 export default App;
--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,22 +1,21 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Chat from "./../pages/Chat";
-import ChatList from "./../pages/ChatList";
-import FriendsList from "./../pages/FriendsList";
-import SignIn from "./../pages/SignIn";
-import SignUp from "./../pages/SignUp";
+import ChatList from "../pages/ChatList";
+import Chat from "../pages/Chat";
+import FriendsList from "../pages/FriendsList";
+import SignIn from "../pages/SignIn";
+import SignUp from "../pages/SignUp";
 import GlobalStyle from "./GlocalStyle";
 const Router = () => {
   return (
     <BrowserRouter>
       <GlobalStyle />
       <Routes>
-        <Route path="/" element={<SignIn />}>
-          <Route path="/friendsList" element={<FriendsList />} />
-          <Route path="/chatList" element={<ChatList />} />
-          <Route path="/chat" element={<Chat />} />
-          <Route path="/signup" element={<SignUp />} />
-        </Route>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/friendsList" element={<FriendsList />} />
+        <Route path="/chatList" element={<ChatList />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );

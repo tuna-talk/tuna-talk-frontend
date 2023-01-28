ea50fbe7dcc71048f85dc75c38cc26f6fb3ae9e4
cheongu <kangsun0528@gmail.com>
2023-01-28 20:19:25 +0900

    axios instance 연

--- src/redux/modules/chatSlice.js
+++ src/redux/modules/chatSlice.js
@@ -26,9 +26,21 @@ export const __getChatListThunk = createAsyncThunk(
   "GET_CHATS",
   async (_, thunkAPI) => {
     try {
-      const { data } = await axiosInstance.get("/chats", config);
-      console.log(data);
-      return thunkAPI.fulfillWithValue(data);
+      const Request = await axiosInstance.get("/chats", config);
+      return thunkAPI.fulfillWithValue(Request.data);
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.code);
+    }
+  }
+);
+
+// 채팅리스트에서 채팅방 삭제요청
+export const __removeChatListThunk = createAsyncThunk(
+  "REMOVE_CHAT",
+  async (payload, thunkAPI) => {
+    try {
+      axiosInstance.delete(`/chats/${payload}`, config);
+      return thunkAPI.fulfillWithValue(payload);
     } catch (e) {
       return thunkAPI.rejectWithValue(e.code);
     }
@@ -39,6 +51,24 @@ export const chatSlice = createSlice({
   name: "chatSlice",
   initialState,
   reducers: {},
-  // extraReducers: {},
+  extraReducers: {
+    [__getChatListThunk.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.chatcollect = action.payload;
+    },
+    [__getChatListThunk.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+    [__getChatListThunk.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [__removeChatListThunk.fulfilled]: (state, action) => {
+      const target = state.chatcollect.findIndex(
+        (chatcollect) => chatcollect.chatRoomId === action.payload
+      );
+      state.chatcollect.splice(target, 1);
+    },
+  },
 });
 export default chatSlice.reducer;

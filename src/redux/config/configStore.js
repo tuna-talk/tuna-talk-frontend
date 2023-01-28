b7bd4169d06195ca4a8e7cf97021012be3d9e34d
jung2322 <vlfjsm1l2@gmail.com>
2023-01-28 21:37:09 +0900

    새로받기위한 커밋

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import chatcollect from "../modules/chatSlice";
+import friendinfoSlice from "../modules/friendInfoSilce";
 
 const store = configureStore({
-  reducer: { chatcollect },
+  reducer: { chatcollect, friendinfoSlice },
 });
 
 export default store;
